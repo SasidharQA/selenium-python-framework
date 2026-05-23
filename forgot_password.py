57c483bbb191c83c7f487835dad6c48717787161
Sasidhar Palleni <Sasidharpalleni101@gmail.com>
2026-05-23 20:21:31 +0530

    Update forgot_password.py

--- forgot_password.py
+++ forgot_password.py
@@ -9,31 +9,26 @@ driver = webdriver.Chrome()
 driver.get("https://automationexercise.com")
 driver.maximize_window()
 
-# Create wait object
 wait = WebDriverWait(driver, 10)
 
-# Click Signup/Login
 wait.until(
     EC.element_to_be_clickable(
         (By.XPATH, "//a[contains(text(),'Signup / Login')]")
     )
 ).click()
 
-# Click Forgot Password
 wait.until(
     EC.element_to_be_clickable(
         (By.XPATH, "//a[contains(text(),'Forgot your password?')]")
     )
 ).click()
 
-# Enter email
 wait.until(
     EC.visibility_of_element_located(
         (By.XPATH, "//input[@type='email']")
     )
 ).send_keys("test@gmail.com")
 
-# Click Submit
 wait.until(
     EC.element_to_be_clickable(
         (By.XPATH, "//button[contains(text(),'Submit')]")
