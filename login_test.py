7458c8167262ef5f81662ee889a4ce49e1eb3348
Sasidhar Palleni <Sasidharpalleni101@gmail.com>
2026-05-23 20:18:54 +0530

    Update login_test.py

--- login_test.py
+++ login_test.py
@@ -4,31 +4,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-# Launch Chrome Browser
 driver = webdriver.Chrome()
 
-# Open Website
 driver.get("https://practicetestautomation.com/practice-test-login/")
 
-# Maximize Browser
 driver.maximize_window()
 
-# Enter Username
 username = driver.find_element(By.ID, "username")
 username.send_keys("student")
 
-# Enter Password
 password = driver.find_element(By.ID, "password")
 password.send_keys("Password123")
 
-# Click Login Button
 login_button = driver.find_element(By.ID, "submit")
 login_button.click()
 
-# Wait for page load
 time.sleep(3)
 
-# Validation
 expected_text = "Logged In Successfully"
 
 if expected_text in driver.page_source:
@@ -36,5 +28,4 @@ if expected_text in driver.page_source:
 else:
     print("Login Test Failed")
 
-# Close Browser
 driver.quit()
