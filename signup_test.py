0451613fae45bb101aed58f5b620c0519cabdd61
Sasidhar Palleni <Sasidharpalleni101@gmail.com>
2026-05-23 20:17:15 +0530

    Update signup_test.py

--- signup_test.py
+++ signup_test.py
@@ -7,44 +7,35 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-# Launch Browser
 driver = webdriver.Chrome()
 
-# Open Website
 driver.get("https://automationexercise.com/signup")
 
-# Maximize Window
 driver.maximize_window()
 
-# Wait Object
 wait = WebDriverWait(driver, 10)
 
 try:
-    # Enter Full Name
     fullname = wait.until(
         EC.presence_of_element_located((By.XPATH, "//input[@name='fullname']"))
     )
     fullname.send_keys("Sasidhar Palleni")
 
-    # Enter Email
+    
     email = driver.find_element(By.XPATH, "//input[@name='email']")
     email.send_keys("sasidhar@gmail.com")
 
-    # Enter Password
     password = driver.find_element(By.XPATH, "//input[@name='password']")
     password.send_keys("Test@123")
 
-    # Confirm Password
     confirm_password = driver.find_element(
         By.XPATH, "//input[@name='confirm_password']")
     confirm_password.send_keys("Test@123")
 
-    # Click Signup Button
     signup_btn = driver.find_element(
         By.XPATH, "//button[text()='Sign Up']")
     signup_btn.click()
-
-    # Validation Message
+    
     success_msg = wait.until(
         EC.presence_of_element_located(
             (By.XPATH, "//div[contains(text(),'successful')]"))
